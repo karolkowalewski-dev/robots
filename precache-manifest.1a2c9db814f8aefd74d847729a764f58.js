75fdb65419a749039c70ec9beec1bb56ba1a24dc
unknown <karolkowalewski@icloud.com>
2021-09-16 15:25:29 +0200

    Updates

---
+++ precache-manifest.1a2c9db814f8aefd74d847729a764f58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20327bdc68d9c203715d5e944d553191",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8ee46618550720721f0a",
+    "url": "/robots/static/css/main.da4d2f4d.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/robots/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robots/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee46618550720721f0a",
+    "url": "/robots/static/js/main.2cbbb9fa.chunk.js"
+  },
+  {
+    "revision": "5c17fe8c4ee59d6a579b",
+    "url": "/robots/static/js/runtime-main.2b834f0c.js"
+  },
+  {
+    "revision": "bad8bfafdb99b73d398cc835523bd620",
+    "url": "/robots/static/media/luckiestguy-regular-webfont.bad8bfaf.woff"
+  }
+]);
