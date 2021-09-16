dcc087df4496856b643ab3795bcdc69740858938
unknown <karolkowalewski@icloud.com>
2021-09-16 15:18:51 +0200

    gh-pages

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc grow bg-light-green br3 pa5 ma2 dib bw2 shadow-5'>
+    <div className='tc grow bg-light-green br3 pa4 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=150x150`} />
       <div>
         <h2>{name}</h2>
